0dc2001fc68df3164d943967b9e0e97295448754
StanislavDitsan <ditsanstas@gmail.com>
2024-03-27 20:55:09 +0100

    React Native app start

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, TouchableOpacity, } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appcontainer}>
+      <View style={styles.inputcontainer}>
+        <TextInput placeholder='Your Goal!' />
+        <Button title='Add Your Goal' />
+      </View>
+      <View>
+        <Text style={styles.listofgoals}>List of Goals</Text>
+      </View>
     </View>
+
   );
 }
 
+// const handlePress = () => {
+//   alert('Button Clicked! You can add more functionality here.');
+// };
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appcontainer:{
+    padding:60,
   },
+  inputcontainer:{
+    flexDirection: 'row',
+  }
+
 });

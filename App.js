b8e6d7e39c2b985dd6652e1c94d7dd77e19be82a
StanislavDitsan <ditsanstas@gmail.com>
2024-03-28 15:47:59 +0100

    Updated list goal padding

--- App.js
+++ App.js
@@ -172,7 +172,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: 5,
+    padding: 10,
     marginBottom: 5,
     borderRadius: 5,
     backgroundColor: 'rgba(255, 255, 255, 0.9)',
